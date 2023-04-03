c2710c8600aa1289b41c11a4bfdd79355d0580b1
Starslayerx <starslayerxleo@gmail.com>
2023-04-03 21:15:47 +0800

    First Page

--- game/views.py
+++ game/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('我的第一个网页!!!')
+    line1 = '<h1 style="text-align: center">我的第一个网页</h1>'
+    line3 = '<hr>'
+    line2 = '<img style="max-width:100%;overflow:hidden;" src="https://s.cn.bing.net/th?id=OHR.HonaunauNP_ZH-CN4491662962_UHD.jpg&rf=LaDigue_UHD.jpg&w=3840&h=2160&c=8&rs=1&o=3&r=0">'
+    return HttpResponse(line1 + line3 + line2)
